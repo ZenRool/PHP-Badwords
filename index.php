61ee324a90e2998f0ea7443651c5434c0c3df441
Zenrool <lorenzopapperini@hotmail.it>
2022-06-07 19:01:43 +0200

    Scaffolding

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php bad words</title>
+</head>
+<body style="text-align: center;">
+    <h1>Bad words</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus doloribus odit, maxime a corporis iste tenetur minima illo eius nostrum ex non. Impedit quam distinctio velit accusamus libero, dolorem minus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius est incidunt doloribus tempore quo blanditiis ad corrupti corporis quae dolores unde debitis, officia natus dolore aspernatur libero explicabo. Aliquam, is. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, explicabo voluptatibus rem suscipit esse vel iste sint voluptatem expedita eius? Velit veniam ipsam, eveniet laboriosam itaque accusamus doloremque. Eaque, odit!</p>
+</body>
+</html>
